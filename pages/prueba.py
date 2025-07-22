d80349426ce1017d7c1ce0a8aa4e9a12607a1366
kaliinversionesyservicios <kaliinversionesyservicios@gmail.com>
2025-07-22 10:52:14 -0500

    creacion de archivo prueba para subir a aws

--- pages/prueba.py
+++ pages/prueba.py
@@ -4,8 +4,14 @@ import numpy as np
 
 
 def app_prueba():
+    url_casos = "/home/ec2-user/data/tba.txt"
+    url_prediccion = "/home/ec2-user/data/prediccion_strike.txt"
     st.title("ESTO ES UN ARCHIVO DE PRUEBA")
-
+    df_prueba=pd.read_csv(url_casos,sep='\t')
+    st.dataframe(df_prueba)
+    st.write("PREDICCION STRIKE")
+    df_prediccion=pd.read_csv(url_prediccion,sep='\t')
+    st.dataframe(df_prediccion)
 
 if __name__=="__main__":
     app_prueba()
