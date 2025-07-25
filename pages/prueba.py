a1b46c7a2c046e8c4085fb33e20c280b94bd262f
kaliinversionesyservicios <kaliinversionesyservicios@gmail.com>
2025-07-25 16:38:09 -0500

    Modificando url de data

--- pages/prueba.py
+++ pages/prueba.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 def app_prueba():
-    url_casos = "/home/ec2-user/data/tba.txt"
-    url_prediccion = "/home/ec2-user/data/prediccion_strike.txt"
+    url_casos = "/home/ubuntu/script/data/tba.txt"
+    url_prediccion = "/home/ubuntu/script/data/prediccion_strike.txt"
     st.title("ESTO ES UN ARCHIVO DE PRUEBA")
     df_prueba=pd.read_csv(url_casos,sep='\t')
     st.dataframe(df_prueba)
